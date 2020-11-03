4c579cd8ca5aa1dca0611a82c88e6597becb4e10
i1rr <i1r@tuta.io>
2020-11-03 21:10:24 +1100

    1.1.1. Конструктор по умолчанию [#368591]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+
+public class Fix {
+    public static void main(String[] args) {
+        Fix fix = new Fix();
+        // Какой-то ерундой занимаемся, поправте урок, про конструкторы ничего дельного
+    }
+}
