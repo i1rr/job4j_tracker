acbd115d9b8b5531c53547be7c87179c14147eac
i1rr <i1r@tuta.io>
2020-11-05 21:31:18 +1100

    6. Date. Отображение даты. [#368594]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,13 +1,16 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
 
+    private LocalDateTime created = LocalDateTime.now();
+
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
@@ -15,8 +18,11 @@ public class Item {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
+
+    public LocalDateTime getCreated() { return created; }
+
+
 }
