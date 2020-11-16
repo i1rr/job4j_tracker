1b2b41b0658f3b6928b97f3144c0ad361edbe34b
i1rr <i1r@tuta.io>
2020-11-16 22:54:22 +1100

    2.1. Реализация класса StartUI [#368545]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,24 +1,75 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
+
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            this.showMenu();
+            int select = Integer.valueOf(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Create a new Item ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+
+            } else if (select == 1) {
+                System.out.println("=== Showing all items ===");
+                tracker.findAll();
+
+            } else if (select == 2) {
+                System.out.println("=== Edit an item ===");
+                System.out.print("Enter ID to replace: ");
+                int id = Integer.valueOf(scanner.nextLine());
+                System.out.println();
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.replace(id, item);
+
+            } else if (select == 3) {
+                System.out.println("=== Delete an item ===");
+                System.out.print("Enter item's ID: ");
+                int id = Integer.valueOf(scanner.nextLine());
+                tracker.delete(id);
+
+            } else if (select == 4) {
+                System.out.println("=== Find an item by ID ===");
+                System.out.print("Enter ID: ");
+                int id = Integer.valueOf(scanner.nextLine());
+                tracker.findById(id);
+
+            } else if (select == 5) {
+                System.out.println("=== Find an item by Name ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                tracker.findByName(name);
+
+            } else if (select == 6) {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        System.out.println("Menu.");
+        System.out.print("0. Add new Item" + System.lineSeparator() +
+                "1. Show all items" + System.lineSeparator() +
+                "2. Edit item" + System.lineSeparator() +
+                "3. Delete item" + System.lineSeparator() +
+                "4. Find item by Id" + System.lineSeparator() +
+                "5. Find items by name" + System.lineSeparator() +
+                "6. Exit Program" + System.lineSeparator() +
+                System.lineSeparator() +
+                "Select: ");
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Tracker tracker = new Tracker();
-        Item vasjaOne = new Item(1, "Vasilij");
-
-        tracker.add(vasjaOne);
-
-        System.out.println(tracker.findById(1));
-
-        /**
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormatter = item.getCreated().format(formatter);
-        System.out.println(currentDateTimeFormatter);
-
-        Item toStringExc = new Item(1, "xpeH");
-        System.out.println(toStringExc);
-       */
+        new StartUI().init(scanner, tracker);
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,55 +14,70 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        return Arrays.copyOf(items, size);
+        Item[] newArray = Arrays.copyOf(items, size);
+        for (Item nA : newArray) {
+            System.out.println(nA);
+        }
+        return newArray;
     }
 
-    public Item[] findByName(String key) {
-        Item[] temp = new Item[size];
-        int tempCounter = 0;
-        for (int i = 0; i < size; i++) {
-            if (items[i].getName().equals(key)) {
-                temp[tempCounter] = items[i];
-                tempCounter++;
+
+        public Item[] findByName (String key){
+            Item[] temp = new Item[size];
+            int tempCounter = 0;
+            for (int i = 0; i < size; i++) {
+                if (items[i].getName().equals(key)) {
+                    temp[tempCounter] = items[i];
+                    System.out.println(items[i]);
+                    tempCounter++;
+                }
             }
-        }
-        return temp;
-    }
-
-    public Item findById(int id) {
-        int index = indexOf(id);
-        return index != -1 ? items[index] : null;
-    }
-
-    private int indexOf(int id) {
-        int rsl = -1;
-        for (int i = 0; i < size; i++) {
-            if(items[i].getId() == id) {
-                rsl = i;
-                break;
+            if(temp.length == 0) {
+                System.out.println(System.lineSeparator() + " === ERROR. Provided NAME does not exist === " + System.lineSeparator());
             }
+            return temp;
         }
-        return rsl;
-    }
 
-    public boolean replace (int id, Item item) {
-        int index = indexOf(id);
-        boolean rsl = index != -1;
-        if(rsl) {
-            items[index] = item;
-            item.setId(id);
+        public Item findById (int id){
+            int index = indexOf(id);
+            System.out.println();
+            System.out.println(index != -1 ? items[index] : System.lineSeparator() + " === ERROR. Provided ID does not exist === " + System.lineSeparator());
+            return index != -1 ? items[index] : null;
         }
-        return rsl;
-    }
 
-    public boolean delete (int id) {
-        int index = indexOf(id);
-        boolean rsl = index != 1;
-        if (rsl) {
-            System.arraycopy(items, index + 1, items, index, size - index);
-            items[size - 1] = null;
-            size--;
+        private int indexOf (int id){
+            int rsl = -1;
+            for (int i = 0; i < size; i++) {
+                if (items[i].getId() == id) {
+                    rsl = i;
+                    break;
+                }
+            }
+            return rsl;
         }
-        return rsl;
+
+        public boolean replace (int id, Item item){
+            int index = indexOf(id);
+            boolean rsl = index != -1;
+            if (rsl) {
+                items[index] = item;
+                item.setId(id);
+            }
+            System.out.println();
+            System.out.println(rsl ? System.lineSeparator() + " === Record UPDATED === " + System.lineSeparator() : System.lineSeparator() + " === ERROR. Provided ID does not exist === " + System.lineSeparator());
+            return rsl;
+        }
+
+        public boolean delete (int id){
+            int index = indexOf(id);
+            boolean rsl = index != -1;
+            if (rsl) {
+                System.arraycopy(items, index + 1, items, index, size - index);
+                items[size - 1] = null;
+                size--;
+            }
+            System.out.println();
+            System.out.println(rsl ? System.lineSeparator() +  " === Record DELETED === " + System.lineSeparator() : System.lineSeparator() + " === ERROR. Provided ID does not exist === " + System.lineSeparator());
+            return rsl;
+        }
     }
-}
