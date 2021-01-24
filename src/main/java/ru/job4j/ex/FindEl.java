38b22a3b799a4f8fbd998894ddfe570074bc5d1e
i1rr <i1r@tuta.io>
2021-01-24 12:00:45 +1100

    0.4. Пользовательские исключения. [#368555]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,29 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key)throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key == value[i]) {
+                rsl = i;
+                break;
+            }
+
+            if (rsl == -1) {
+                throw new ElementNotFoundException("Requested element is missing");
+            }
+        }
+
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] tryCatch = {"Ku", "Ka", "Ra", "Cha"};
+        try {
+            indexOf(tryCatch, "Ko");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
