562b334d0f00229d0676537ba3a137dcf9074378
i1rr <i1r@tuta.io>
2021-02-14 12:13:59 +1100

    2. Очередь с приоритетом на LinkedList [#368513] fix

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -1,7 +1,7 @@
 package ru.job4j.ooa;
 
 public final class Airbus extends Aircraft {
-        private static int countEngine = 2;
+        private static int COUNT_ENGINE = 2;
 
         private String name;
 
@@ -23,8 +23,8 @@ public final class Airbus extends Aircraft {
         }
 
         public void printCountEngine() {
-            countEngine = 4;
-            System.out.println("Количество двигателей равно: " + countEngine);
+            COUNT_ENGINE = 4;
+            System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
         }
 
         @Override
--- src/main/java/ru/job4j/search/Task.java
+++ src/main/java/ru/job4j/search/Task.java
@@ -1,3 +1,4 @@
+
 package ru.job4j.search;
 
 public class Task {
