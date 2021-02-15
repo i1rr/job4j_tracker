cb4e00abce927c43f889e84e82d0e5d70446753d
i1rr <i1r@tuta.io>
2021-02-15 19:24:23 +1100

    1. Паспорт и жители [#368521]

---
+++ src/main/java/ru/job4j/collection/Citizen.java
@@ -0,0 +1,33 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class Citizen {
+    private String passport, tovarisch;
+
+    public Citizen(String passport, String tovarisch) {
+        this.passport = passport;
+        this.tovarisch = tovarisch;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public String getUsername() {
+        return tovarisch;
+    }
+
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Citizen citizen = (Citizen) o;
+        return Objects.equals(passport, citizen.passport);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(passport);
+    }
+}
