64237c2ea3cb2211837e226fc8cf82fabb895825
i1rr <i1r@tuta.io>
2021-02-27 11:17:43 +1100

    Collections. Exam. Text generator v2

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -2,12 +2,17 @@ package ru.job4j.collection;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        HashSet<String> words = new HashSet<>(Arrays.asList(origin.split(" |,|;|:|!|[.]")));
-        int trick = words.size();
-        words.addAll(Arrays.asList(line.split(" |,|;|:|!|[.]")));
-        return trick == words.size();
+        Set<String> or = new HashSet<>(Arrays.asList(origin.split(" |,|;|:|!|[.]")));
+        Set<String> li = new HashSet<>(Arrays.asList(line.split(" |,|;|:|!|[.]")));
+        for (String aWord : li) {
+            if (!or.contains(aWord)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
