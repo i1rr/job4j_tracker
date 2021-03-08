6ace7cb2f78c587e82699ffe346b64d1db313e42
i1rr <i1r@tuta.io>
2021-03-08 21:58:41 +1100

    2.0. Встроенные функциональные интерфейсы. [#368340]

---
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -0,0 +1,32 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchAtt {
+
+    public static List<Attachment> filterSize(List<Attachment> list) {
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if (att.getSize() > 100) {
+                rsl.add(att);
+            }
+        }
+        return rsl;
+    }
+
+    public static List<Attachment> filterName(List<Attachment> list) {
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if (att.getName().contains("bug")) {
+                rsl.add(att);
+            }
+        }
+        return rsl;
+    }
+
+    public static List<Attachment> uniFilter(List<Attachment> list, Predicate<Attachment> pred) {
+        return filterName(filterSize(list)); //это просто чтоб айдийка не ругалась. Я задания не понимаю, что хотят от меня.
+    }
+}
