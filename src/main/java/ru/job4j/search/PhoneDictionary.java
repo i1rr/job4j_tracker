3d0b845ef60a74df294bcc36786fb6f184b4a792
i1rr <i1r@tuta.io>
2021-03-14 14:02:55 +1100

    6. Higher order function.

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,26 +1,27 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
-    private ArrayList<Person> persons = new ArrayList<Person>();
+    private ArrayList<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
         this.persons.add(person);
     }
 
     public ArrayList<Person> find(String key) {
-        ArrayList<Person> result = new ArrayList<Person>();
+        Predicate<Person> checkName = x -> x.getName().contains(key);
+        Predicate<Person> checkLastName = x -> x.getLastName().contains(key);
+        Predicate<Person> checkPhone = x -> x.getPhone().contains(key);
+        Predicate<Person> checkAddress = x -> x.getAddress().contains(key);
+        Predicate<Person> combine = checkName.or(checkLastName).or(checkPhone).or(checkAddress);
+        ArrayList<Person> rsl = new ArrayList<>();
         for (Person gg : persons) {
-            if (
-                       gg.getName().contains(key)
-                    || gg.getLastName().contains(key)
-                    || gg.getPhone().contains(key)
-                    || gg.getAddress().contains(key)
-            ) {
-                result.add(gg);
+            if (combine.test(gg)) {
+                rsl.add(gg);
             }
         }
-        return result;
+        return rsl;
     }
 }
