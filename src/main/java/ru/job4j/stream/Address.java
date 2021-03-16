05923292ab96321d73e794bcbc3bfd1bb7a486e5
i1rr <i1r@tuta.io>
2021-03-16 21:51:46 +1100

    3. Unique sorting of elements.

--- src/main/java/ru/job4j/stream/Address.java
+++ src/main/java/ru/job4j/stream/Address.java
@@ -13,9 +13,33 @@ public class Address {
         this.unit = unit;
     }
 
+    public String getCity() {
+        return city;
+    }
+
     @Override
     public String toString() {
         return unit + "/" + building + " "
                 + street + ". " + city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        return building == address.building
+                && unit == address.unit
+                && Objects.equals(city, address.city)
+                && Objects.equals(street, address.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, building, unit);
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.stream;
 
 import org.junit.Test;
-
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
@@ -13,11 +12,15 @@ public class ProfilesTest {
     public void collect() {
         Address sydney = new Address("Sydney", "Napoleon st", 74, 0);
         Address china = new Address("Pekin", "Shun dzhu", 7, 19293);
+        Address russia = new Address("St. Petersburg", "Ulica lenina", 3, 14);
         List<Profile> input = List.of(
                 new Profile(sydney),
-                new Profile(china)
+                new Profile(china),
+                new Profile(sydney),
+                new Profile(china),
+                new Profile(russia)
         );
         Profiles pro = new Profiles();
-        assertThat(pro.collect(input), is(List.of(sydney, china)));
+        assertThat(pro.collect(input), is(List.of(china, russia, sydney)));
     }
 }
